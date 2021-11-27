65c3cce065dab89d076a57036d742380c58a42dd
mateusalvesf <88463010+mateusalvesf@users.noreply.github.com>
2021-11-27 10:41:19 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+import random
+import numpy as np
+
+print("Loterias Quina")
+
+print("Sorteado")
+x = np.random.randint(1,52, (5))
+
+print(x)
+
+
+print("Seus Numeros")
+y = np.random.randint(1,52, (5))
+
+
+print(y)
